d3182d153ad25e6e1f5a26cf8ef53108c688f0de
Yukinyaaa <65931718+Yukinyaaa@users.noreply.github.com>
2023-07-28 20:40:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import rl from "readline-sync";
 import Parser from "./modules/parser.js";
 let parser = new Parser();
 
+console.clear();
 while(1) {
     let data = rl.question("< ");
     let res = parser.parse(data);
